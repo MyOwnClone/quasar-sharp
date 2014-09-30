4116acb07e22d3d4d7359e026a2d2f69c9e78f69
Sloan Crandell <grunt@localhost.localdomain>
2014-09-30 06:21:40 -0400

    Minor commit

--- QuasarVM/Main.cs
+++ QuasarVM/Main.cs
@@ -10,22 +10,39 @@ namespace GruntXProductions.Quasar.VM
 		public static void Main (string[] args)
 		{
 			string input = null;
-			uint ramsize = 0xFFFFFF;
-			QuasarRam memory = new QuasarRam(ramsize);
-			Emulator emu = new Emulator(memory);
-			
+			uint ramsize = 0x20000000;
+			bool debug = false;
 			for(int i = 0; i < args.Length; i++)
 			{
 				if(!args[i].StartsWith("-") && input == null)
 					input = args[i];
 				else if (args[i] == "-S")
 				{
-					DebugServer dbg = new DebugServer(emu);
-					dbg.Start();
+					debug = true;
+				}
+				else if (args[i] == "-m")
+				{
+					string amount = args[++i];
+					uint rs;
+					if(uint.TryParse(amount, out rs)) 
+					{
+						ramsize = rs * 0x100000;
+					}
+					else
+						Fail("Size {0} is not valid!", amount);
 				}
 			}
 			
+			if(input == null)
+				Fail("No input specified!");
 			
+			QuasarRam memory = new QuasarRam(ramsize);
+			Emulator emu = new Emulator(memory);
+			if(debug)
+			{
+				DebugServer dbg = new DebugServer(emu);
+				dbg.Start();
+			}
 			byte[] program = File.ReadAllBytes(input);
 			memory.Write(0, program.Length, 0, program);
 			DeviceSerialController serialController = new DeviceSerialController();
@@ -42,6 +59,12 @@ namespace GruntXProductions.Quasar.VM
 			}).Start();
 			
 			System.Windows.Forms.Application.Run(form);
+		}
+		
+		private static void Fail(string format, params object[] args)
+		{
+			Console.Error.WriteLine(format, args);
+			Environment.Exit(-1);
 		}
 	}
 }
